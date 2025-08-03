52630d23a2499097b933a0b9d319c0c5d2ec8a2d
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-03 13:28:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
